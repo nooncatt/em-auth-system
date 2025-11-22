cc813133905f0110a8d5065aa8130b835cedb29b
AA Karpenko <anastasha.me@gmail.com>
2025-11-22 18:39:44 +0200

    added classes RegisterSerializer, UserSerializer. updated class RegisterView, added LoginView in views.py

--- config/users/views.py
+++ config/users/views.py
@@ -1,3 +1,24 @@
+from http import HTTPStatus
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+
+# /api/auth/register
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            data = UserSerializer(user).data
+            return Response(data, status=HTTPStatus.CREATED)
+        return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+
+
+def LoginView(APIView):
+    def post(self, request):
+        pass
